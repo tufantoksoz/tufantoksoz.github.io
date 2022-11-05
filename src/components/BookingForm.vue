7402753c27ab36eae457be0d039a037ac5ba5cf2
Tufan <tufantoksozz@gmail.com>
2022-11-05 03:43:09 +0300

    update form

--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -2,58 +2,109 @@
   <div class="q-pa-md" style="max-width: 400px">
     <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
       <q-input
+        readonly
+        filled
+        v-model="date"
+        input-class="cursor-pointer"
+        mask="##/##/####"
+      >
+        <q-popup-proxy ref="qDateProxy" :breakpoint="0" behavior="menu">
+          <q-date
+            v-model="date"
+            minimal
+            no-unset
+            mask="MM/DD/YYYY"
+            :options="options"
+            @update:model-value="$refs.qDateProxy.hide()"
+          >
+          </q-date>
+        </q-popup-proxy>
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer"></q-icon>
+        </template>
+      </q-input>
+
+      <div class="row">
+        <q-input
+          class="q-pr-xs"
+          v-model.number="model"
+          type="number"
+          filled
+          label="Adult *"
+          style="width: 50%"
+          min="0"
+          max="20"
+        />
+
+        <q-input
+          class="q-pl-xs"
+          v-model.number="model"
+          type="number"
+          filled
+          label="Child *"
+          style="width: 50%"
+          min="0"
+          max="20"
+        />
+      </div>
+
+      <q-input
         filled
         v-model="name"
         label="Your name *"
-        hint="Name and surname"
         lazy-rules
-        :rules="[(val) => (val && val.length > 0) || 'Please type something']"
+        :rules="[(val) => (val && val.length > 0) || 'Please type your name']"
       />
 
       <q-input
         filled
-        type="number"
-        v-model="age"
-        label="Your age *"
+        v-model="email"
+        type="email"
+        label="E-mail"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Please type your e-mail']"
+      />
+
+      <q-input
+        filled
+        v-model="phone"
+        type="tel"
+        label="Phone Number"
         lazy-rules
         :rules="[
-          (val) => (val !== null && val !== '') || 'Please type your age',
-          (val) => (val > 0 && val < 100) || 'Please type a real age',
+          (val) => (val && val.length > 0) || 'Please type your phone number',
         ]"
       />
 
-      <q-toggle v-model="accept" label="I accept the license and terms" />
-
       <div>
         <q-btn label="Submit" type="submit" color="primary" />
-        <q-btn
-          label="Reset"
-          type="reset"
-          color="primary"
-          flat
-          class="q-ml-sm"
-        />
       </div>
     </q-form>
   </div>
 </template>
 
 <script>
-import { useQuasar } from 'quasar';
+import { useQuasar, date } from 'quasar';
 import { ref } from 'vue';
 
 export default {
   setup() {
     const $q = useQuasar();
 
+    const timeStamp = Date.now();
+
     const name = ref(null);
-    const age = ref(null);
+    const email = ref(null);
+    const phone = ref(null);
+
     const accept = ref(false);
 
     return {
       name,
-      age,
+      email,
+      phone,
       accept,
+      date: ref(date.formatDate(timeStamp, 'MM/DD/YYYY')),
 
       onSubmit() {
         if (accept.value !== true) {
@@ -73,10 +124,8 @@ export default {
         }
       },
 
-      onReset() {
-        name.value = null;
-        age.value = null;
-        accept.value = false;
+      options(d) {
+        return d >= date.formatDate(timeStamp, 'YYYY/MM/DD');
       },
     };
   },
