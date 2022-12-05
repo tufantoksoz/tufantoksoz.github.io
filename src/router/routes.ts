c02d2f5d67f2f5a3c90bd547d0596389d01693cf
Tufan <tufantoksozz@gmail.com>
2022-12-06 02:37:52 +0300

    update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -14,47 +14,47 @@ const routes: RouteRecordRaw[] = [
       },
     ],
   },
-  // {
-  //   path: '/antalya',
-  //   component: MainLayout,
-  //   children: [
-  //     {
-  //       path: '',
-  //       name: 'antalya',
-  //       component: () => import('pages/category/antalya/AntalyaCategory.vue'),
-  //     },
-  //     {
-  //       path: 'belek',
-  //       name: 'belek',
-  //       component: () => import('pages/category/antalya/BelekCategory.vue'),
-  //     },
-  //     {
-  //       path: 'kemer',
-  //       name: 'kemer',
-  //       component: () => import('pages/category/antalya/KemerCategory.vue'),
-  //     },
-  //     {
-  //       path: 'alanya',
-  //       name: 'alanya',
-  //       component: () => import('pages/category/antalya/AlanyaCategory.vue'),
-  //     },
-  //     {
-  //       path: 'manavgat',
-  //       name: 'manavgat',
-  //       component: () => import('pages/category/antalya/ManavgatCategory.vue'),
-  //     },
-  //     {
-  //       path: 'side',
-  //       name: 'side',
-  //       component: () => import('pages/category/antalya/SideCategory.vue'),
-  //     },
-  //     {
-  //       path: 'adrasan',
-  //       name: 'adrasan',
-  //       component: () => import('pages/category/antalya/AdrasanCategory.vue'),
-  //     },
-  //   ],
-  // },
+  {
+    path: '/antalya',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        name: 'antalya',
+        component: () => import('pages/category/antalya/AntalyaCategory.vue'),
+      },
+      {
+        path: 'belek',
+        name: 'belek',
+        component: () => import('pages/category/antalya/BelekCategory.vue'),
+      },
+      {
+        path: 'kemer',
+        name: 'kemer',
+        component: () => import('pages/category/antalya/KemerCategory.vue'),
+      },
+      {
+        path: 'alanya',
+        name: 'alanya',
+        component: () => import('pages/category/antalya/AlanyaCategory.vue'),
+      },
+      {
+        path: 'manavgat',
+        name: 'manavgat',
+        component: () => import('pages/category/antalya/ManavgatCategory.vue'),
+      },
+      {
+        path: 'side',
+        name: 'side',
+        component: () => import('pages/category/antalya/SideCategory.vue'),
+      },
+      {
+        path: 'adrasan',
+        name: 'adrasan',
+        component: () => import('pages/category/antalya/AdrasanCategory.vue'),
+      },
+    ],
+  },
 
   // Always leave this as last one, but you can also remove it
   {
